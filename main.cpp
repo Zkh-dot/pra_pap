4e65c0bb9307cb244f207aa1966faffa71f9a081
BMSTU-tda <76221301+BMSTU-tda@users.noreply.github.com>
2020-12-18 19:51:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,56 +5,56 @@ using namespace std;
 int menu();
 
 int main() {
-    system("chcp 1251 >nul");
-    setlocale(LC_ALL, "");
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    char* txt = 0;
-    Text text;
-    char* word = 0;
-    char ch = 'x';
-    while (true) {
-        switch (menu()) {
-        case 1: cout << "Âûïîëíåíî äåéñòâèå 1";
-            break;
-        case 2: text = GetText(txt);
-            cout << text << endl;
-            break;
-        case 3: cout << "Âûïîëíåíî äåéñòâèå 3";
-            break;
-        case 4: cout << "Âûïîëíåíî äåéñòâèå 4";
-            break;
-        //  <âûçîâ ôóíêöèè, ðåàëèçóþùåé âàðèàíò äðóãîãî ÷ëåíà ãðóïïû>
-        case 5: cout << "Âûïîëíåíî äåéñòâèå 5";
-            break;
-        case 6: delete[] txt; return 0;
-        default: cout << ("Íàäî ââîäèòü ÷èñëà îò 1 äî 6\n");
-            break;
-        }
-    }
-    system("pause");
-    return 0;
+	system("chcp 1251 >nul");
+	setlocale(LC_ALL, "");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char* txt = 0;
+	Text text;
+	char* word = 0;
+	char ch = 'x';
+	while (true) {
+		switch (menu()) {
+		case 1: cout << "Выполнено действие 1";
+			break;
+		case 2: text = GetText(txt);
+			cout << text << endl;
+			break;
+		case 3: cout << "Выполнено действие 3";
+			break;
+		case 4: cout << "Выполнено действие 4";
+			break;
+		//  <вызов функции, реализующей вариант другого члена группы>
+		case 5: cout << "Выполнено действие 5";
+			break;
+		case 6: delete[] txt; return 0;
+		default: cout << ("Надо вводить числа от 1 до 6\n");
+			break;
+		}
+	}
+	system("pause");
+	return 0;
 }
 
 //---------------------------------------------------------------------
 int menu() {
-    cout << "===================ÄÎÌÀØÍÅÅ ÇÀÄÀÍÈÅ===================\n";
-    cout << "\t1 - Ââîä òåêñòà ñ êëàâèàòóðû\n";
-    cout << "\t2 - Çàïîëíåíèå ñòðóêóðû Text\n";
-    cout << "\t3 - Çàïîëíåíèå ñòðóêóðû Sentence\n";
-    cout << "\t4 - ÄÇ âàðèàíò 18\n";
-    cout << "\t5 - ÄÇ âàðèàíò 12\n";
-    cout << "\t6 - Âûõîä\n";
-    int choice;
-    cout << "Âûáåðåòå äåéñòâèå: ";
-    cin >> choice;
-    while (cin.fail()) {
-        cout << "Îøèáêà ââîäà. Ïîâòîðèòå ââîä\n";//rohbornb
-        cin.clear();
-        cin.ignore(10, '\n');
-        cout << "Âûáåðåòå äåéñòâèå: ";
-        cin >> choice;
-    }
-    return choice;
+	cout << "===================ДОМАШНЕЕ ЗАДАНИЕ===================\n";
+	cout << "\t1 - Ввод текста с клавиатуры\n";
+	cout << "\t2 - Заполнение струкуры Text\n";
+	cout << "\t3 - Заполнение струкуры Sentence\n";
+	cout << "\t4 - ДЗ вариант 18\n";
+	cout << "\t5 - ДЗ вариант 12\n";
+	cout << "\t6 - Выход\n";
+	int choice;
+	cout << "Выберете действие: ";
+	cin >> choice;
+	while (cin.fail()) {
+		cout << "Ошибка ввода. Повторите ввод\n";
+		cin.clear();
+		cin.ignore(10, '\n');
+		cout << "Выберете действие: ";
+		cin >> choice;
+	}
+	return choice;
 }
-//----------------------------------------------------------------------
+//---------------------------------------------------------------------
